b56b115ef3c0ef1f9f9d37ac1a4b38a1c39f052d
svalentini-hub <sorinvalentinionas@gmail.com>
2026-08-31 10:55:38 +0300

    remove duplicate files from root, keep only day01 copies

--- greetings.py
+++
@@ -1,8 +0,0 @@
-#greeting script
-name = input("Hello, what is your name? ")
-
-age = int(input("How old are you? "))
-#had to use int to convert the answer(a string) to an int(integer=number)
-age_future = age + 10
-
-print(f"Hello {name}, you will be {age_future} in 10 years")
