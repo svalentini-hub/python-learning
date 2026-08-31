cf5242f13e4e717a610c4ea8decead4a824d2ee6
svalentini-hub <sorinvalentinionas@gmail.com>
2026-08-31 10:51:59 +0300

    Day 1 of learning
    Started learning python again and doing scripts without tutorials, only looking up answers to errors I am getting in my code.

---
+++ day01/tempconverter.py
@@ -0,0 +1,6 @@
+print("Welcome to the Temperature Converter!")
+
+temp_celsius = float(input("Please give me the temperature in Celsius: "))
+temp_fahrenheit = temp_celsius * 9/5 + 32
+
+print(f"The temperature from celsius {temp_celsius}, has been converted to fahrenheit {temp_fahrenheit}! ")
